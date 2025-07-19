095755a7bc6c53f983713d76b5e8793e84654caa
romain-lelaidier <romain.lelaidier@etu.minesparis.psl.eu>
2025-07-19 21:49:45 +0200

    yti_player logger

--- server/youtube_extractor.js
+++ server/youtube_extractor.js
@@ -327,6 +327,7 @@ class YTMClient {
                 )
             ])
             .then(data => {
+                console.log(JSON.stringify(data[0]), id, this.INNERTUBE_CLIENT_VERSION)
                 var info = {
                     id: data[0].videoDetails.videoId,
                     title: data[0].videoDetails.title,
